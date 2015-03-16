ec9f92c7b80e7e5e179d212903ca2deb4f789960
Fallonious <FallonSkinner@gmail.com>
2015-03-16 16:51:04 -0400

    add Google embedded map api - no functionality yet

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,7 @@
 class Search < ActiveRecord::Base
   has_many :landmarks
+
+  def show_map
+    puts HTTParty.get("https://www.google.com/maps/embed/v1/search?key=#{ENV['GOOGLE_KEY']}&q=record+stores+in+Seattle")
+  end
 end
